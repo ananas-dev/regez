8fb7a2ba38c2bf1c71606d5e9bf43a30521b1094
Lucien Fiorini <lucien.fiorini@student.uclouvain.be>
2024-04-12 23:01:10 +0200

    implement debug for bitset

--- src/bitset.rs
+++ src/bitset.rs
@@ -1,6 +1,6 @@
-use std::fmt::{Debug, Display};
+use std::fmt::Debug;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct BitSet<'a, T> {
     universe: &'a [T],
     v: Vec<u64>,
@@ -12,6 +12,15 @@ impl<'a, T: PartialEq> PartialEq for BitSet<'a, T> {
         self.v == other.v && self.size == other.size
     }
 }
+
+impl<'a, T: PartialEq + Copy + Debug> Debug for BitSet<'a, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_set()
+            .entries(self.universe.iter().filter(|&&x| self.contains(x)))
+            .finish()
+    }
+}
+
 
 impl<'a, T: PartialEq + Copy + Debug> BitSet<'a, T> {
     pub fn empty(universe: &'a [T]) -> BitSet<'a, T> {
@@ -39,6 +48,13 @@ impl<'a, T: PartialEq + Copy + Debug> BitSet<'a, T> {
             v,
             size
         }
+    }
+
+    pub fn contains(&self, element: T) -> bool {
+        if let Some(index) = self.universe.iter().position(|&x| x == element) {
+            return self.v[index / 64] & 1 << (index % 64) != 0;
+        }
+        false
     }
 
     pub fn insert(&mut self, element: T) {
